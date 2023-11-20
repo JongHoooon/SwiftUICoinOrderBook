7265e51272851ec8bec51729814c581a1cfc76c4
JongHoon <qnm541@gmail.com>
2023-11-20 11:46:45 +0900

    Initial Commit

---
+++ SwiftUICoinOrderBook/SwiftUICoinOrderBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICoinOrderBookApp.swift
+//  SwiftUICoinOrderBook
+//
+//  Created by JongHoon on 11/20/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICoinOrderBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
